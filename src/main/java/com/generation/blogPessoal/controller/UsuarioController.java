6b4147f4d946e9c509b5bf13f5cbbbf87c9579e8
Rafaela <rafaelazaccarias@gmail.com>
2021-06-09 01:26:50 -0300

    atualizando blog pessoal

--- src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
@@ -1,22 +1,29 @@
 package com.generation.blogPessoal.controller;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.generation.blogPessoal.model.UserLogin;
 import com.generation.blogPessoal.model.Usuario;
+import com.generation.blogPessoal.repository.UsuarioRepository;
 import com.generation.blogPessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
 
 	@Autowired
 	private UsuarioService usuarioService;
@@ -33,4 +40,9 @@ public class UsuarioController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
 	}	
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -31,6 +31,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/{id}").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
